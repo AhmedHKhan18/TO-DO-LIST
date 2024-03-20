92b0fcb63eefbde7d70fcbed05f5fc8473084b6c
AhmedHKhan18 <ahmed.18hassan.18@gmail.com>
2024-03-20 10:40:47 +0500

    updated

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 var input = document.getElementById("input");
 var list = document.getElementById("list");
+var thirddiv = document.getElementsByClassName("thirddiv");
 
 function add() {
-    if (input.value == "") {
-        alert("Please enter a task");
-        return;
-    }else {
-        var task = input.value;
-        list.innerHTML += `<li onclick="remove(this)">${task} <button>Delete</button></li>`;
-        input.value = "";
+    if (thirddiv.length < 5) {
 
+        if (input.value == "") {
+            alert("Please enter a task");
+            return;
+        }else {
+            var task = input.value;
+            list.innerHTML += `<div class="thirddiv" onclick="remove(this)"><h3>${task}</h3> <button><img src="/assets/bin.png"></button></div>`;
+            input.value = "";
+    
+        }
+    }else {
+        alert("You have reached the maximum number of tasks");
     }
+
 }
 
 function remove(element) {
